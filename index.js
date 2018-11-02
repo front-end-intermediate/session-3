faf62b5f314a19dffe4ed62719f63002616a5273
DannyBoyNYC <daniel.deverell@gmail.com>
2018-11-02 17:29:36 -0400

    done

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const fs = require('fs');
 
-const url = 'mongodb://dannyboynyc:dd2345@ds139969.mlab.com:39969/bcl'
+const mongourl = 'mongodb://dannyboynyc:dd2345@ds139969.mlab.com:39969/bcl'
 
 const articles = [];
 
@@ -18,17 +18,37 @@ var content = fs.readFile('./json/travel.json', { encoding: 'utf8' }, function (
   })
 })
 
-
+MongoClient.connect(mongourl, (err, client) => {  
+  if (err) {
+    return console.log(err);
+  }
+  db = client.db('bcl')
+  // Do something with db here, like inserting a record
+  db.collection('notes').insertOne(
+    {
+      title: 'Hello MongoDB',
+      text: 'Hopefully this works!'
+    },
+    function (err, res) {
+      if (err) {
+        client.close();
+        return console.log(err);
+      }
+      // Success
+      client.close();
+    }
+  )
+});
 
 // app.use(express.static('app'));
 
-app.post('/entries', (req, res) => {
-  db.collection('entries').save(req.body, (err, result) => {
-    if (err) return console.log(err);
-    console.log('saved to database');
-    res.redirect('/');
-  });
-});
+// app.post('/entries', (req, res) => {
+//   db.collection('entries').save(req.body, (err, result) => {
+//     if (err) return console.log(err);
+//     console.log('saved to database');
+//     res.redirect('/');
+//   });
+// });
 
 // our first route
 app.get('/', (req, res) => {
@@ -47,11 +67,11 @@ app.get('/:article', function (req, res) {
   res.send(article)
 });
 
-MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
-  if (err) return console.log(err);
-  const db = client.db;
+// MongoClient.connect(mongourl, { useNewUrlParser: true }, (err, db) => {
+//   if (err) return console.log(err);
+  // db = db;
   app.listen(port, () => {
     console.log(`Listening on port ${port}!`);
   });
-});
+// });
 
