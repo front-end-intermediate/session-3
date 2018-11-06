4ec09e6c65f4ea8f054651de15a3d0a93d677a99
DannyBoyNYC <daniel.deverell@gmail.com>
2018-11-05 19:30:10 -0500

    done

--- other/script.js
+++ other/script.js
@@ -1,7 +1,15 @@
 // node script.js
+const http = require('http');
 
-var addItems = function (num1, num2) {
-  console.log( num1 + num2 )
-}
+const hostname = '127.0.0.1';
+const port = 3000;
 
-addItems(1,2)
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
