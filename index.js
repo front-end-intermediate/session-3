42104b990013272b00a55108db18bb2421bc6755
DannyBoyNYC <daniel.deverell@gmail.com>
2018-11-05 20:33:37 -0500

    done

--- index.js
+++ index.js
@@ -1,34 +1,27 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
 const fs = require('fs');
+
 const app = express();
 const port = 9000;
 const log = console.log;
 const articles = [];
 
 app.use(express.static('app'));
-
-var content = fs.readFile('./other/json/travel.json', { encoding: 'utf8' }, function (err, data) {
-  if (err) throw err
-  JSON.parse(data).forEach(function (article) {
-    var story = {}
-    story.title = article.title;
-    story.abstract = article.abstract;
-    story.image = Math.floor(Math.random() * 3 +1)
-    articles.push(story);
-  })
-  log(articles)
-})
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // our first route
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-app.get(/Oslo.*/, function (req, res, next){
-  console.log('OSLO');
-  const location = 'Norway';
-  res.location = location;
-  next()
+app.post('/entries', (req, res) => {
+  db.collection('entries').insertOne(req.body, (err, result) => {
+    if (err) return log(err)
+    log('saved to database')
+    res.redirect('/');
+  })
 })
 
 // our third route
@@ -43,16 +36,41 @@ app.get('/:article', function (req, res) {
   res.send(buffer);
 });
 
-// our second route
-app.get('/music/:type', function(req, res) {
-  const reverse = [...req.params.type].reverse().join(''); // NEW
-  let type = reverse;
-    res.send(`
-    <h1>Music</h1>
-    <p>Commentary on ${type} music will go here.</p>
-    `);
+MongoClient.connect(
+  'mongodb://dannyboynyc:dd2345@ds139969.mlab.com:39969/bcl', { useNewUrlParser: true }, (err, client) => {
+  if (err) return console.log(err);
+  db = client.db('bcl');
+  app.listen(port, () => {
+    console.log(`Listening on port ${port}!`);
+  });
 });
 
-app.listen(port, function() {
-  console.log(`Listening on localhost port ${port}!`);
-});
+// var content = fs.readFile('./other/json/travel.json', { encoding: 'utf8' }, function (err, data) {
+//   if (err) throw err
+//   JSON.parse(data).forEach(function (article) {
+//     var story = {}
+//     story.title = article.title;
+//     story.abstract = article.abstract;
+//     story.image = Math.floor(Math.random() * 3 +1)
+//     articles.push(story);
+//   })
+//   log(articles)
+// })
+
+// app.get(/Oslo.*/, function (req, res, next){
+//   console.log('OSLO');
+//   const location = 'Norway';
+//   res.location = location;
+//   next()
+// })
+
+
+// our second route
+// app.get('/music/:type', function(req, res) {
+//   const reverse = [...req.params.type].reverse().join(''); // NEW
+//   let type = reverse;
+//     res.send(`
+//     <h1>Music</h1>
+//     <p>Commentary on ${type} music will go here.</p>
+//     `);
+// });
